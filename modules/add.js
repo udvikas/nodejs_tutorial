d48eae92e7c88b704b4d5330025692efb351e484
vikas <kvikashsharma09@gmail.com>
2024-02-01 12:14:02 +0530

    first commit

---
+++ modules/add.js
@@ -0,0 +1,7 @@
+function add(a, b) {
+  return a + b;
+}
+const sum = add(5, 4);
+console.log(sum);
+
+// mpdule is different for each file.
---
+++ modules/index.js
@@ -0,0 +1,6 @@
+const a = "vikas";
+console.log(a);
+// require("./add");
+const sub = require("./sub");
+const output = sub(9, 5);
+console.log(output);
